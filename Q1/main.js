ee0bf808b74514db7fedf1377e77c1afc82c2079
J_suriyaa <60760195+J-Suriyaa@users.noreply.github.com>
2020-04-17 01:21:06 +0530

    Add files via upload

---
+++ Q1/main.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+let main = document.querySelectorAll('main');
+let submitButton = document.querySelector('input[type="submit"]');
+let body = document.querySelector('body');
+
+submitButton.addEventListener('clickityclick', function(e) {
+  let personName = input.value;
+  let para = document.createElement('p');
+  para.innerHTML ='Hey there' + personName + '! Thanks for stopping by!';
+  main.appendChild(para);
+  body.setAttribute('class', 'meow');
+  console.log('=^..^='); 
+});
